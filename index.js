e8a23e9d778f8abfd42a70be51743c52dc65b961
Evan McKinney-Robinson <evan.mckinney-robinson@theverygroup.com>
2023-09-27 13:23:26 +0100

    Moved index.js to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import './index.css';
+import 'src/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from 'src/App';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
