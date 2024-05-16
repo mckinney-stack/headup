19e181bd075ce84cf566e0fa65e77ae1f9f75e43
Evan McKinney-Robinson <evan.mckinney-robinson@theverygroup.com>
2024-05-16 12:30:25 +0100

    Add intellisense for name input from existing database entries

--- src/UserName.js
+++ src/UserName.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { getDatabase, ref, get } from "firebase/database";
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 
@@ -16,6 +18,20 @@ const UserHeading = styled.label`
 const UserInput = styled.input`
   margin-left: 8px;
   margin-right: -8px;
+  background-color: transparent;
+  color: black;
+  position: relative;
+  border-radius: 4px;
+`;
+
+const UserInputShadow = styled.input`
+  margin-left: 8px;
+  margin-right: -8px;
+  background-color: transparent;
+  color: lightgrey;
+  position: absolute;
+  pointer-events: none;
+  border-radius: 4px;
 `;
 
 function handleSubmit(e) {
@@ -23,12 +39,52 @@ function handleSubmit(e) {
 }
 
 export default function UserName({userName, setUserName}) {
-    return ( 
+
+  const [userNames, setUserNames] = useState([]);
+  const [suggestion, setSuggestion] = useState('');
+
+  useEffect(() => {
+    const db = getDatabase();
+    get(ref(db, 'users')).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const names = Object.keys(data);
+        setUserNames(names);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, [userNames]);
+
+  useEffect(() => {
+    if (userName) {
+      const match = userNames.find(name => name.startsWith(userName));
+      setSuggestion(match ? match : '');
+    } else {
+      setSuggestion('');
+    }
+  }, [userName, userNames]);
+
+  return ( 
     <UserNameContainer onSubmit={handleSubmit}>
-    <UserHeading>
-      <FormattedMessage id="player" />
-      <UserInput type="text" value={userName} onChange={e => setUserName(e.target.value)} />
-    </UserHeading>
-  </UserNameContainer>
+      <UserHeading>
+        <FormattedMessage id="player" />
+        <UserInputShadow type="text" value={suggestion} disabled />
+        <UserInput 
+        type="text" 
+        value={userName} 
+        onChange={e => setUserName(e.target.value)} 
+        onKeyDown={e => {
+          if (e.key === 'Tab') {
+            e.preventDefault();
+            setUserName(suggestion);
+          }
+        }}
+        onClick={() => {if (suggestion) {setUserName(suggestion)}}}
+        />
+      </UserHeading>
+    </UserNameContainer>
   );
 };
